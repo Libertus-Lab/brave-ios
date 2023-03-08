fd6e639b11f2a61808735efb759685a7a7cce164
Nuo Xu <nuoxu.nx@gmail.com>
2023-03-07 22:06:11 -0500

    WIP ipfs navigation in omnibox

--- App/iOS/Delegates/AppDelegate.swift
+++ App/iOS/Delegates/AppDelegate.swift
@@ -434,7 +434,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       (SessionRestoreHandler.path, SessionRestoreHandler()),
       (ErrorPageHandler.path, ErrorPageHandler()),
       (ReaderModeHandler.path, ReaderModeHandler(profile: profile)),
-      (SNSDomainHandler.path, SNSDomainHandler())
+      (SNSDomainHandler.path, SNSDomainHandler()),
+      (IPFSSchemeHandler.path, IPFSSchemeHandler())
     ]
     responders.forEach { (path, responder) in
       InternalSchemeHandler.responders[path] = responder
